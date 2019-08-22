2a1af0a538e00e4eccdef237298932f40f7372e5
Stefan Larsson <stefan@lastsys.com>
2019-08-22 10:34:14 +0200

    Added AoC2015 - Day04.

---
+++ AoC2015_Day04.fs
@@ -0,0 +1,47 @@
+﻿module AoC2015.Day04
+
+open NUnit.Framework
+open FsUnit
+open System.Security.Cryptography
+
+let mine (leadingZeroes : int) (secretKey : string) =
+    let md5 = MD5.Create()
+
+    let indexOfFirstNonZero (s : string) =
+        s.ToCharArray()
+        |> Seq.takeWhile ((=) '0')
+        |> Seq.length
+        
+    let rec findNumber (number : int) =
+        let hash =
+            secretKey + number.ToString()
+            |> System.Text.Encoding.ASCII.GetBytes
+            |> md5.ComputeHash
+            |> Array.map (fun x -> System.String.Format("{0:X2}", x))
+            |> String.concat System.String.Empty
+        if indexOfFirstNonZero hash = leadingZeroes
+        then number
+        else findNumber (number + 1)
+        
+    findNumber 0
+
+let part1 input =
+    mine 5 input
+    |> printfn "AOC2015 - Day04 - Part 1: %d"
+
+let part2 input =
+    mine 6 input
+    |> printfn "AOC2015 - Day04 - Part 2: %d"
+
+let run =
+    let key = "bgvyzdsv" 
+    part1 key
+    part2 key
+
+[<Test>]
+let ``Part 1 - Example 1`` () =
+    mine 5 "abcdef" |> should equal 609043
+    
+[<Test>]
+let ``Part 1 - Example 2`` () =
+    mine 5 "pqrstuv" |> should equal 1048970
--- Program.fs
+++ Program.fs
@@ -1,6 +1,8 @@
 ﻿[<EntryPoint>]
 let main argv =
+    printfn "*** Advent of Code in FSharp ***"
     AoC2015.Day01.run
     AoC2015.Day02.run
     AoC2015.Day03.run
+    AoC2015.Day04.run
     0 // return an integer exit code
